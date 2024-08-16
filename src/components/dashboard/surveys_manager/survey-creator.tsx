c92ae2f801803c70d4933adb2a299b4ec50432f9
User <quangdatcqd@gmail.com>
2024-08-16 21:57:35 +0700

    ok

--- src/components/dashboard/surveys_manager/survey-creator.tsx
+++ src/components/dashboard/surveys_manager/survey-creator.tsx
@@ -1,27 +1,29 @@
 import "survey-core/defaultV2.min.css";
 import "survey-creator-core/survey-creator-core.min.css";
-import { SurveyCreatorComponent, SurveyCreator } from "survey-creator-react";
-
+import { SurveyCreatorComponent, SurveyCreator } from "survey-creator-react"; 
 
 const creatorOptions = {
     // showLogicTab: true,
-    // questionTypes: ["text", "checkbox", "radiogroup", "dropdown" ],
+    questionTypes: ["text", "checkbox", "radiogroup", "dropdown", "yesno"],
     isAutoSave: true,
     showJSONEditorTab: false,
-    // showLogicTab: false,
-    // showPreviewTab: true,
-    // showTitle: true,
-    // showQuestionNumbers: "on",
-    // showOptionsCaption: true,
-    // showOptionsCaptionOnSelect: true,
-    // showDesignerTab: false
+    showLogicTab: false,
+    showPreviewTab: true,
+    showTitle: true,
+    showQuestionNumbers: "on",
+    showOptionsCaption: true,
+    showOptionsCaptionOnSelect: true,
+    showDesignerTab: false
 
 };
 export default function SurveyCreatorWidget() {
-    const creator = new SurveyCreator(creatorOptions);
-   
-  
+    const creator = new SurveyCreator(creatorOptions); 
+    creator.saveSurveyFunc = (saveNo:any, callback:any) => {
+        // If you use localStorage:
+        window.localStorage.setItem("survey-json", creator.text);
+        callback(saveNo, true); 
+    };
+
 
     return (<SurveyCreatorComponent creator={creator}   />)
-  
 }
