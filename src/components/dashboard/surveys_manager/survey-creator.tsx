5abc8ec73116d598888ab868d3a440f661b2ccfd
User <quangdatcqd@gmail.com>
2024-08-18 13:34:06 +0700

    ok

--- src/components/dashboard/surveys_manager/survey-creator.tsx
+++ src/components/dashboard/surveys_manager/survey-creator.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "survey-core/defaultV2.min.css";
 import "survey-creator-core/survey-creator-core.min.css";
 import { SurveyCreatorComponent, SurveyCreator } from "survey-creator-react";
@@ -13,14 +14,17 @@ const creatorOptions = {
 };
 export default function SurveyCreatorWidget({ survey }: any) {
     const creator = new SurveyCreator(creatorOptions);
-    if (survey) creator.JSON = JSON.parse(survey || "{}");
-    else if (survey === false) localStorage.setItem("survey-json", "{}");
-    else creator.JSON = JSON.parse(localStorage.getItem("survey-json") || "{}");
+    React.useEffect(() => {
+        if (survey) creator.JSON = JSON.parse(survey || "{}");
+        else if (survey === false) localStorage.setItem("survey-json", "{}");
+        else creator.JSON = JSON.parse(localStorage.getItem("survey-json") || "{}");
+        creator.saveSurveyFunc = (saveNo: any, callback: any) => {
+            localStorage.setItem("survey-json", creator.text);
+            callback(saveNo, true);
+        };
 
-    creator.saveSurveyFunc = (saveNo: any, callback: any) => {
-        localStorage.setItem("survey-json", creator.text);
-        callback(saveNo, true);
-    };
+    }, [])
+
 
     return (<SurveyCreatorComponent creator={creator} />)
 }
